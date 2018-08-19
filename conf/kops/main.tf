4c3522da4bf2dff2ca717e825c9a609237e808fd
Erik Osterman <erik@cloudposse.com>
2018-08-19 09:55:44 -0700

    Intialize kiosk distribution (#1)
    * Intialize kiosk distribution
    
    * Update image name to kiosk
    
    * fix markdown
    
    * Update instructions
    
    * Update README
    
    * Update installation instructions for WSL
    
    * Cluster orchestration scaffolding
    
    * Add logo
    
    * Change logo
    
    * Use SVG
    
    * Add targets for helm and helmfile
    
    * Use envs
    
    * Almost fully automated cluster provisioning
    
    * Define missing envs. Upgrade geodesic.
    
    * Upgrade root modules
    
    * import root modules
    
    * add tfstate backend
    
    * remove role arn
    
    * Generalize secrets create
    
    * Remove `--recreate-pods`
    
    * Update `geodesic` version
    
    * Update `build-harness` version
    
    * Update `README`

---
+++ conf/kops/main.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_version = ">= 0.11.2"
+
+  backend "s3" {}
+}
+
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Name  (e.g. `kops`)"
+  default     = "kops"
+}
+
+variable "region" {
+  type        = "string"
+  description = "AWS region"
+}
+
+variable "zone_name" {
+  type        = "string"
+  description = "DNS zone name"
+}
+
+variable "domain_enabled" {
+  type        = "string"
+  description = "Enable DNS Zone creation for kops"
+  default     = "true"
+}
+
+variable "force_destroy" {
+  type        = "string"
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without errors. These objects are not recoverable."
+  default     = "false"
+}
+
+variable "ssh_public_key_path" {
+  type        = "string"
+  description = "SSH public key path to write master public/private key pair for cluster"
+  default     = "/secrets/tf/ssh"
+}
+
+provider "aws" {
+}
+
+module "kops_state_backend" {
+  source           = "git::https://github.com/cloudposse/terraform-aws-kops-state-backend.git?ref=tags/0.1.5"
+  namespace        = "${var.namespace}"
+  stage            = "${var.stage}"
+  name             = "${var.name}"
+  attributes       = ["state"]
+  cluster_name     = "${var.region}"
+  parent_zone_name = "${var.zone_name}"
+  zone_name        = "$${name}.$${parent_zone_name}"
+  domain_enabled   = "${var.domain_enabled}"
+  force_destroy    = "${var.force_destroy}"
+  region           = "${var.region}"
+}
+
+module "ssh_key_pair" {
+  source              = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=tags/0.2.3"
+  namespace           = "${var.namespace}"
+  stage               = "${var.stage}"
+  name                = "${var.name}"
+  attributes          = ["${var.region}"]
+  ssh_public_key_path = "${var.ssh_public_key_path}"
+  generate_ssh_key    = "true"
+}
