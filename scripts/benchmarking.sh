037d2bc49999fa6f8b1f9cd6ded506ec6a30966c
dylanbannon <dylanbannon5@gmail.com>
2019-01-16 19:41:23 -0800

    debugging benchmarking menu selection

--- scripts/benchmarking.sh
+++
@@ -1,5 +0,0 @@
-export BENCHMARKING_PU_TYPE_AND_NUMBER=$(echo $BENCHMARK_TYPE | cut -f2 -d',' | sed 's/-/ /')
-export BENCHMARKING_PU_TYPE=$(echo $BENCHMARKING_PU_TYPE_AND_NUMBER | cut -f2 -d' ')
-export img_num=$(echo $BENCHMARK_TYPE | grep -o '[0-9]\+-image' | grep -o '[0-9]\+')
-
-# scale up number of benchmarking pods from 0 to 1?
