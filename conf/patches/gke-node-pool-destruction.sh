ff19702e9643694b3355710e5c5eaeb2905611ba
dylanbannon <dylanbannon5@gmail.com>
2019-03-31 22:08:06 -0800

    fixing variable reference typo

--- conf/patches/gke-node-pool-destruction.sh
+++ conf/patches/gke-node-pool-destruction.sh
@@ -1,5 +1,5 @@
 echo "Destroying prediction GPU node pool..."
-until gcloud container node-pools delete prediction-gpu --region $(GPU_COMPUTE_REGION)
+until gcloud container node-pools delete prediction-gpu --region ${GPU_COMPUTE_REGION}
 do
     echo "Prediction GPU node pool destruction failed. Trying again in 30 seconds."
     sleep 30
@@ -7,7 +7,7 @@ done
 echo "Prediction GPU node pool destruction finished."
 
 echo "Destroying training GPU node pool..."
-until gcloud container node-pools delete training-gpu --region $(GPU_COMPUTE_REGION)
+until gcloud container node-pools delete training-gpu --region ${GPU_COMPUTE_REGION}
 do
     echo "Training GPU node pool destruction failed. Trying again in 30 seconds."
     sleep 30
@@ -15,7 +15,7 @@ done
 echo "Training GPU node pool destruction finished."
 
 echo "Destroying elasticsearch CPU node pool..."
-until gcloud container node-pools delete elasticsearch-cpu --region $(GKE_COMPUTE_REGION)
+until gcloud container node-pools delete elasticsearch-cpu --region ${GKE_COMPUTE_REGION}
 do
     echo "Elasticsearch-data CPU node pool destruction failed. Trying again in 30 seconds."
     sleep 30
@@ -23,7 +23,7 @@ done
 echo "Elasticsearch CPU node pool destruction finished."
 
 echo "Destroying redis-consumer, data-processing CPU node pool..."
-until gcloud container node-pools delete rc-dp-cpu --region $(GKE_COMPUTE_REGION)
+until gcloud container node-pools delete rc-dp-cpu --region ${GKE_COMPUTE_REGION}
 do
     echo "Redis-consumer, data-processing CPU node pool destruction failed. Trying again in 30 seconds."
     sleep 30
