bdcc1842f80a7e9f76f4eba1a7f65231da8df824
ajt0ng <ajtong@caltech.edu>
2019-07-01 10:55:26 -0700

    using radiobox input

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -245,10 +245,7 @@ function configure_gke() {
   if [ "$CLUSTER_NAME" = "" ]; then
 	  return 0
   fi
-  export GKE_BUCKET=$(inputbox "Deepcell" "Bucket Name
-
-  The bucket should be a unique existing bucket on google cloud. It acts as a storage area for models, data, and more.
-  If you do not have one, first create a bucket under storage on google cloud" "${GKE_BUCKET:-invalid_default}")
+  export GKE_BUCKET=$(inputbox "Deepcell" "Bucket Name (needs to be a unique name)" "${GKE_BUCKET:-invalid_default}")
   if [ "$GKE_BUCKET" = "" ]; then
 	  return 0
   fi
@@ -259,8 +256,7 @@ function configure_gke() {
   local selector_box_lines=$(echo "${regions}" | tr -cd '\n' | wc -c)
   local total_lines=$(($base_box_height + $selector_box_lines))
   export GKE_COMPUTE_REGION=$(radiobox "Google Cloud" \
-      "Choose a region for hosting your cluster:
-      Click space to choose your region and enter to continue" \
+      "Choose a region for hosting your cluster:" \
 	  $total_lines 60 $selector_box_lines "$regions_with_default")
   if [ "$GKE_COMPUTE_REGION" = "" ]; then
 	  return 0
@@ -285,13 +281,11 @@ function configure_gke() {
   local selector_box_lines=$(($(echo "${gpus_in_region}" | tr -cd '\n' | wc -c) + 1))
   local total_lines=$(($base_box_height + $selector_box_lines))
   export PREDICTION_GPU_TYPE=$(radiobox "Google Cloud" \
-      "Choose a GPU for prediction (not training) from the GPU types available in your region:
-      Click space to choose and enter to continue" \
+      "Choose a GPU for prediction (not training) from the GPU types available in your region:" \
 	  $total_lines 60 $selector_box_lines "$gpus_with_default")
 
   export TRAINING_GPU_TYPE=$(radiobox "Google Cloud" \
-      "Choose a GPU for training (not prediction) from the GPU types available in your region:
-      Click space to choose and enter to continue" \
+      "Choose a GPU for training (not prediction) from the GPU types available in your region:" \
 	  $total_lines 60 $selector_box_lines "$gpus_with_default")
 
   local zones=$(gcloud compute zones list | grep "${GKE_COMPUTE_REGION}" | grep "UP" | awk '{print $1 " _ OFF"}')
