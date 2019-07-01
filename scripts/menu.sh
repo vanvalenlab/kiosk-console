699841f02bc77a734a06627af69e2ff97ed29701
ajt0ng <ajtong@caltech.edu>
2019-07-01 10:37:18 -0700

    using radiobox input

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -265,7 +265,7 @@ function configure_gke() {
   export GKE_BUCKET=$(inputbox "Deepcell" "Bucket Name
 
   The bucket should be a unique existing bucket on google cloud. It acts as a storage area for models, data, and more.
-  If you do not have one, first create a bucket under storage on google cloud" "${GKE_BUCKET:-invalid_default}")# -60 -13
+  If you do not have one, first create a bucket under storage on google cloud" "${GKE_BUCKET:-invalid_default}")
   if [ "$GKE_BUCKET" = "" ]; then
 	  return 0
   fi
