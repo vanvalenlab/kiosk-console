067de33907ce40e787c6465ded578a43b52f41e2
William Graf <7930703+willgraf@users.noreply.github.com>
2019-08-26 12:16:49 -0700

    Add a period to "Press the spacbar"

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -257,7 +257,7 @@ function configure_gke() {
   local selector_box_lines=$(echo "${regions}" | tr -cd '\n' | wc -c)
   local total_lines=$(($base_box_height + $selector_box_lines))
   export GKE_COMPUTE_REGION=$(radiobox "Google Cloud" \
-      "Choose a region for hosting your cluster: \nPress the spacebar to select and Enter to continue" \
+      "Choose a region for hosting your cluster: \nPress the spacebar to select and Enter to continue." \
 	  $total_lines 60 $selector_box_lines "$regions_with_default")
   if [ "$GKE_COMPUTE_REGION" = "" ]; then
 	  return 0
@@ -282,11 +282,11 @@ function configure_gke() {
   local selector_box_lines=$(($(echo "${gpus_in_region}" | tr -cd '\n' | wc -c) + 1))
   local total_lines=$(($base_box_height + $selector_box_lines))
   export PREDICTION_GPU_TYPE=$(radiobox "Google Cloud" \
-      "Choose a GPU for prediction (not training) from the GPU types available in your region: \nPress the spacebar to select and Enter to continue" \
+      "Choose a GPU for prediction (not training) from the GPU types available in your region: \nPress the spacebar to select and Enter to continue." \
 	  $total_lines 60 $selector_box_lines "$gpus_with_default")
 
   export TRAINING_GPU_TYPE=$(radiobox "Google Cloud" \
-      "Choose a GPU for training (not prediction) from the GPU types available in your region: \nPress the spacebar to select and Enter to continue" \
+      "Choose a GPU for training (not prediction) from the GPU types available in your region: \nPress the spacebar to select and Enter to continue." \
 	  $total_lines 60 $selector_box_lines "$gpus_with_default")
 
   local zones=$(gcloud compute zones list | grep "${GKE_COMPUTE_REGION}" | grep "UP" | awk '{print $1 " _ OFF"}')
