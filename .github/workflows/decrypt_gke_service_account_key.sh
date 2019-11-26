ad177f5a688f711fca9e2ba57722f4ba2bb2aa5b
dylanbannon <dylanbannon5@gmail.com>
2019-11-25 17:49:31 -0800

    complete key decryption workflow

---
+++ .github/workflows/decrypt_gke_service_account_key.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$GKE_KEY_PASSPHRASE" \
+--output $HOME/secrets/gke_service_account_key.json gke_service_account_key.json.gpg
