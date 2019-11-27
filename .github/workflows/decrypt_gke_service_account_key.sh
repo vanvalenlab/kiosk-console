8f3fb22417b08eb02e6a0424c497210a82808fa7
dylanbannon <dylanbannon5@gmail.com>
2019-11-27 12:19:37 -0800

    debugging

--- .github/workflows/decrypt_gke_service_account_key.sh
+++ .github/workflows/decrypt_gke_service_account_key.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GKE_KEY_PASSPHRASE" \
---output $HOME/secrets/gke_service_account_key_base64.json ./.github/workflows/gke_service_account_key_base64.json.gpg
+--output $HOME/secrets/gke_service_account_key.json ./.github/workflows/gke_service_account_key.json.gpg
