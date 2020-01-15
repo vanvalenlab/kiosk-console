266da96d7e3ef6bdf3d861124ad8d8f84f279af1
William Graf <7930703+willgraf@users.noreply.github.com>
2020-01-14 18:46:00 -0800

    convert shell scripts from while loop to for loop to avoid infinite retry bug

--- conf/patches/gke-helmfile-destruction.sh
+++ conf/patches/gke-helmfile-destruction.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 for filename in /conf/helmfile.d/*.yaml; do
-    echo ${filename}
-    #echo 'grep "\- name: " ${filename} | grep -m1 -v "\- name: \"stable\"" | awk {print \$3} | sed s/^\"\(.\+\)\"$/\1/'
     deployment_name=$(grep "\- name: " ${filename} | grep -m1 -v "\- name: \"stable\"" | awk '{print $3}' | sed 's/^\"\(.\+\)\"$/\1/')
-    echo ${deployment_name}
-    until helm delete ${deployment_name} --purge; do
+    retries=4
+    for ((i=0; i<retries; i++)); do
+        helm delete ${deployment_name} --purge
+        [[ $? -eq 0 ]] && break
+        echo "Something went wrong while deleteing ${deployment_name}. Retrying in 30 seconds."
         sleep 30
     done
 done
