e4f28513138ce756ed80d04b20f85b4ea1271cb7
MekWarrior <moen.erick@gmail.com>
2020-01-23 12:06:51 -0800

    Upgrade and streamline kiosk creation/destruction menu. (#218)
    * Update `confirm` to load a yesno dialog box.
    
    * Add confirm destroy action.
    
    * Add default creation settings flow.
    
    * Add extra loading screens in advanced configuration flow.
    
    * improve language for `gcloud-login.sh` and silence `gcloud config project` command.
    
    * correct ordering of h/w in all dialog boxes to match bash std.
    
    * remove old comments and improve whitespace.
    
    * use multi-line strings to clean up messages, de-duplicate zone warning.
    
    * convert tabs to spaces.
    
    Co-authored-by: willgraf <7930703+willgraf@users.noreply.github.com>

--- scripts/gcloud-login.sh
+++ scripts/gcloud-login.sh
@@ -5,9 +5,14 @@ long_url=$(gcloud auth login --no-launch-browser </dev/null 2>&1 | grep https://
 short_url=$(curl -sS "http://tinyurl.com/api-create.php?url=$long_url")
 clear
 # qrencode -t utf8 "$long_url"
-echo "Please go here: $long_url"
+echo "Deploying a cluster on Google Cloud requires authorization.
 
-echo -n "Enter code: "
+Please go to this website to authorize access to your Google Cloud account:
+
+$long_url
+"
+
+echo -n "Enter authorization code: "
 read code
 
 echo "$code" | gcloud auth login --no-launch-browser --quiet >/dev/null 2>&1
