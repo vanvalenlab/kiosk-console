4787a73371737ae028e8c3e0a1606632e106b8f9
Dylan Bannon <dylanbannon5@gmail.com>
2020-02-25 15:57:24 -0800

    disabling github actions tests

--- .github/workflows/decrypt_gke_service_account_key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$GKE_KEY_PASSPHRASE" \
---output $HOME/secrets/gke_service_account_key.json ./.github/workflows/gke_service_account_key.json.gpg
--- .github/workflows/write_gke_service_account_key_to_file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir $HOME/secrets
-output_file=$HOME/secrets/gke_service_account_key.json
-echo "$GKE_KEY_BASE64" > "$output_file"
