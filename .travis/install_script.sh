6e117dd8fa3f48dacfc06eea74c3ba1cc3df2337
William Graf <7930703+willgraf@users.noreply.github.com>
2020-04-01 11:04:33 -0700

    install yq for testing. [build-integration-tests]
    Co-Authored-By: willgraf <7930703+willgraf@users.noreply.github.com>
    Co-Authored-By: MekWarrior <moen.erick@gmail.com>

--- .travis/install_script.sh
+++ .travis/install_script.sh
@@ -27,7 +27,9 @@ sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
-## prep directory structure for gcloud	
+# Install yq
+sudo apt-get update && sudo apt-get install yq
+## prep directory structure for gcloud
 mkdir -p $CONF_PATH_PREFIX/.config/gcloud
 sudo chmod 777 $CONF_PATH_PREFIX/.config
 sudo chmod 777 $CONF_PATH_PREFIX/.config/gcloud
