b2a57744f13dbb596e0c2e9bfbc6711bf874a18a
William Graf <7930703+willgraf@users.noreply.github.com>
2020-05-15 15:37:09 -0700

    debugging

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -505,7 +505,7 @@ function configure_gke() {
   fi
 
   # TODO: del this msgbox
-  msgbox "REGION_ZONES_WITH_GPUS" "${REGION_ZONES_WITH_GPUS}." 7 55
+  msgbox "REGION_ZONES_WITH_GPUS" "${REGION_ZONES_WITH_GPUS}\n${valid_zones[*]}." 7 55
 
   msgbox "Configuration Complete!" "\nThe cluster is now available for creation." 7 55
 
