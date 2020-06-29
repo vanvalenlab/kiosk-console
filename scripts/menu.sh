ee51d42972d76a900a9bb51a26d9e09d9eb63d6e
willgraf <7930703+willgraf@users.noreply.github.com>
2020-06-29 14:56:01 -0700

    Warn the user that multizone clusters will have additional egress fees. (#366)
    * Warn the user that multizone clusters will have additional egress fees.

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -503,7 +503,10 @@ function configure_gke() {
     valid_zones+=('Multizone')  # add an "All of the above option"
   fi
 
-  local message=("Deploy a single- or multi-zone cluster.")
+  local message=("Deploy a single- or multi-zone cluster.\n"
+                 "\nMultiple zones provide more redundancy, but will incur additional egress fees. For more details, see:"
+                 "\n\n"
+                 "https://cloud.google.com/vpc/network-pricing")
   # The default version of 1.14 is the oldest supported version, and may become
   # unavailable in GKE in the future.
   if [[ $KUBERNETES_VERSION == "1.14" ]] && \
