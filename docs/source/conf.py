0634e232de3fb0ec5d1ec0b2b6df52b03a72a43d
William Graf <7930703+willgraf@users.noreply.github.com>
2020-12-09 17:00:53 -0800

    Set default READTHEDOCS_VERSION to master.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ release = '1.0.0'
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 # This is used for linking and such so we link to the thing we're building
-rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
+rtd_version = os.environ.get("READTHEDOCS_VERSION", "master")
 if rtd_version not in ["master", "latest", "stable"]:
     rtd_version = "master"
 
@@ -111,7 +111,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
