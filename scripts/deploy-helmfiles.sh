1a4aad0ef73e0ad2f9c29f8973b507b0fabd85e2
willgraf <7930703+willgraf@users.noreply.github.com>
2020-12-14 13:13:36 -0800

    Update helmfile defaults for faster helm deployments. (#404)
    * Update comments and reference URLs for all helmfiles.
    
    * Remove default helm options and define them for each release. Use `cleanupOnFail` and `atomic` for all releases.
    
    * Update timeouts for each release: Redis, prometheus, and openvpn keep their 600s timeout, all others go to 300s (except cluster-issuers, 180s)
    
    * Add exponential delay to helmfile deployment and log all failures. (Fixes #349)
    
    * Rename script to `deploy-helmfiles.sh` as there is nothing GKE specific.
    
    * Reduce noise from kubens in create task.

---
+++ scripts/deploy-helmfiles.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+failures=()
+((base_time = 30))
+retries=3
+
+for filename in /conf/helmfile.d/*.yaml; do
+  deployment_names=$(helmfile -f $filename build | \
+                     yq r - -- releases[*].name | awk '{print $NF}')
+  for name in $deployment_names; do
+    for ((i=0; i<retries; i++)); do
+      # Try to deploy and break out of retry loop if successful
+      helmfile --selector name=${name} sync
+      [[ $? -eq 0 ]] && break
+
+      # If we still have retries, sleep for a bit and retry
+      # Otherwise, add deployment to list of failures
+      if [ $(($i+1)) -lt $retries ]; then
+        ((time = $base_time * ($i + 1)))
+        echo "Something went wrong while deploying ${name}. Retrying in ${time} seconds."
+        echo " "
+        sleep $time
+      else
+        failures+=($name)
+        echo " "
+        echo "Failed to deploy ${name}!"
+        echo " "
+      fi
+
+    done
+  done
+done
+
+# Log all the failed deployments
+if [ ${#failures[@]} -gt 0 ]; then
+  echo ""
+  echo "Some deployments failed!"
+  echo "Please re-deploy the failures by running the following commands from the Shell:"
+  echo ""
+  for failure in ${failures[@]}; do
+    echo "    helmfile -l name=${failure} sync"
+  done
+  echo ""
+  echo "Helmfile deployment finished with errors."
+else
+  echo ""
+  echo "Helmfile deployment finished."
+fi
